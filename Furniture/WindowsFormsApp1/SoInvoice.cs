ee6658cbd0804fd2de14148b968777b6bf8a86dd
ramalchamuditha <36332204+ramalchamuditha@users.noreply.github.com>
2018-08-18 20:18:35 +0530

    cache

--- Furniture/WindowsFormsApp1/SoInvoice.cs
+++ Furniture/WindowsFormsApp1/SoInvoice.cs
@@ -24,7 +24,7 @@ namespace WindowsFormsApp1
             label4.Text = DateTime.Now.ToLongDateString();
             label6.Text = DateTime.Now.ToLongTimeString();
 
-                    }
+        }
 
 
         private void btn_payment_Click(object sender, EventArgs e)
